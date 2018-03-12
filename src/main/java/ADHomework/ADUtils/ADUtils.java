6a2bea6c2512ff900fce767d070283557f393ec2
Kazimieras P <kazimieras.pociunas@gmail.com>
2018-03-13 00:44:07 +0200

    now handles multiple clicks for one view, still slow

--- src/main/java/ADHomework/ADUtils/ADUtils.java
+++ src/main/java/ADHomework/ADUtils/ADUtils.java
@@ -9,14 +9,13 @@ import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.text.ParseException;
+import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
-import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -42,10 +41,10 @@ public class ADUtils {
             try {
                 return new ADClick(line);
             } catch (ParseException e) {
-                System.out.printf("Could not parse line \"%s\" to ADHomework.ADEntities.ADClick.\n", line);
+                //System.out.printf("Could not parse line \"%s\" to ADHomework.ADEntities.ADClick.\n", line);
                 return null;
             } catch (NumberFormatException e) {
-                System.out.printf("Could not parse line \"%s\" to ADHomework.ADEntities.ADClick.\n", line);
+                //System.out.printf("Could not parse line \"%s\" to ADHomework.ADEntities.ADClick.\n", line);
                 return null;
             }
         }
@@ -55,51 +54,40 @@ public class ADUtils {
 
     }
 
-    public static void getADViewsWithClicks(Stream<String> views, Path clicksPath, StatefulBeanToCsv beanToCsv) throws CsvDataTypeMismatchException, CsvRequiredFieldEmptyException {
+    public static void getADViewsWithClicks(Stream<String> views, Path clicksPath, StatefulBeanToCsv beanToCsv) {
 
         views.map(lineToADView)
                 .filter(adView -> adView != null)
                 //TODO: need to filter all garbage then act on good adViews only
-                .map(enrichADView(clicksPath))
+                .flatMap(adView -> includeClicks(clicksPath, adView))
                 .filter(adViewWithClick -> adViewWithClick.getClickId() != null)
                 .forEach(writeToCsv(beanToCsv));
     }
 
-    private static Function<ADView, ADViewWithClick> enrichADView(Path clicks) {
-        return adView -> new ADViewWithClick(adView.getId(), adView.getLogTime(), getClickId(clicks, adView.getId()));
-    }
-
-    private static Long getClickId(Path clicksPath, Long viewId)  {
+    private static Stream<ADViewWithClick> includeClicks(Path clicksPath, ADView adView) {
         BufferedReader clicks = null;
         try {
             clicks = Files.newBufferedReader(clicksPath);
+            System.out.printf("Handling ADView.id %s \n",adView.getId().toString());
+            return clicks.lines()
+                    .map(lineToADClick)
+                    .filter(adClick -> adClick != null)
+                    .filter(adClick -> adView.getId().compareTo(adClick.getInteractionId()) == 0)
+                    .map(adClick -> new ADViewWithClick(adView.getId(), adView.getLogTime(), adClick.getId()));
         } catch (IOException e) {
             System.out.printf("Failed to create new reader for Path %s \n", clicksPath.toString());
         }
-        return clicks.lines()
-                .map(lineToADClick)
-                .filter(adClick -> adClick != null)
-                .map(adClick -> adClick.getInteractionId())
-                //.filter(compareLongWithMessedUpScale(viewId))
-                .filter(interactionId -> {
-                    if (interactionId == 4781245516087271927l) {System.out.printf("clickId %s viewId %s \n", interactionId, viewId);} //TODO: delete this later
-                    return interactionId == viewId;
-                })
-                .findFirst()
-                .orElse(null);
-    }
-
-    private static Predicate<Long> compareLongWithMessedUpScale(Long id) {
-        return intId -> (new BigDecimal(id / 10000)).setScale(0, RoundingMode.UP)
-                .compareTo(new BigDecimal(intId / 10000).setScale(0, RoundingMode.UP)) == 0;
+        return null;
     }
 
     private static Consumer<ADViewWithClick> writeToCsv(StatefulBeanToCsv beanToCsv) {
         return o -> {
             try {
                 beanToCsv.write(o);
-            } catch (Exception e) {
-                System.err.println(e.getMessage());
+            } catch (CsvDataTypeMismatchException dtm) {
+                System.out.printf("Data type mismatch for %s \n", o.toString());
+            } catch (CsvRequiredFieldEmptyException rfe) {
+                System.out.printf("Required field missing for %s \n", o.toString());
             }
             ;
         };
